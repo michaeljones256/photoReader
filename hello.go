f8621fabef7740aecafcf87c75e8d2c6022fdae4
Michael Jones <michael.jones1890@gmail.com>
2023-12-26 21:45:01 -0500

    seperate functions

--- hello.go
+++ hello.go
@@ -3,42 +3,57 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"sync"
 	"time"
 )
 
 var m sync.Mutex
 
-func recurseDirectories(wg *sync.WaitGroup, photoPaths *[]string, directory string) {
+func recurseDirectoriesMultiThread(wg *sync.WaitGroup, photoPaths *[]string, directory string) {
 	defer wg.Done()
 	items, _ := os.ReadDir(directory)
 	for _, item := range items {
 		if item.IsDir() {
-			fmt.Println(item.Name())
 			wg.Add(1)
-			go recurseDirectories(wg, photoPaths, directory+"/"+item.Name())
+			go recurseDirectoriesMultiThread(wg, photoPaths, directory+"/"+item.Name())
 		} else {
-			// handle file there
+			// handle file
 			m.Lock()
 			*photoPaths = append(*photoPaths, directory+"/"+item.Name())
 			m.Unlock()
 		}
 	}
 }
+func recurseDirectories(photoPaths *[]string, directory string) {
+	items, _ := os.ReadDir(directory)
+	for _, item := range items {
+		if item.IsDir() {
+			recurseDirectories(photoPaths, directory+"/"+item.Name())
+		} else {
+			// handle file
+			*photoPaths = append(*photoPaths, directory+"/"+item.Name())
+		}
+	}
+}
 
 func main() {
 	args := os.Args
-	fmt.Printf("Type of Args = %T\n", args)
-	fmt.Println(args[0], args[1])
+	fmt.Println(args[1])
 	path := args[1]
 
-	stringSlice := make([]string, 0) // make creates slices
+	stringSlice1 := make([]string, 0) // slice
+	start := time.Now()
+	recurseDirectories(&stringSlice1, path)
+	fmt.Println("Single thread time " + time.Since(start).String())
+	fmt.Println("Number of files " + strconv.Itoa(len(stringSlice1)))
 
+	stringSlice2 := make([]string, 0) // slice
 	wg := new(sync.WaitGroup)
 	wg.Add(1)
-	start := time.Now()
-	recurseDirectories(wg, &stringSlice, path)
+	start = time.Now()
+	recurseDirectoriesMultiThread(wg, &stringSlice2, path)
 	wg.Wait()
-	fmt.Println(time.Since(start))
-	fmt.Println(len(stringSlice))
+	fmt.Println("Mulit thread time " + time.Since(start).String())
+	fmt.Println("Number of files " + strconv.Itoa(len(stringSlice2)))
 }
